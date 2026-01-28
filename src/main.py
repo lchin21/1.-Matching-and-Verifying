89c60fbff36d216c6d3a23a1a4d3d1f1790073d5
lchin <l.chin@ufl.edu>
2026-01-28 17:05:51 -0500

    string to ints

--- src/main.py
+++ src/main.py
@@ -4,14 +4,15 @@ def main():
     student_preferences: list[list[int]] = []
     
     for i in range(n):
-        preferences: list[int] = input().strip().split(" ")
+        preferences: list[int] = [int(n) for n in input().strip().split(" ")]
         hospital_preferences.append(preferences)
         
     for i in range(n):
-        preferences: list[int] = input().strip().split(" ")
+        preferences: list[int] = [int(n) for n in input().strip().split(" ")]
         student_preferences.append(preferences)
     
     # call matcher
     # call validator
     
-main()
+if __name__ == "__main__":
+    main()
