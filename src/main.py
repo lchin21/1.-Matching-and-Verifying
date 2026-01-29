16dadea6020f232f6371fe63abc7d342dc587e9a
davispham1 <davispham1126@gmail.com>
2026-01-29 10:37:45 -0500

    testing and calling matcher

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
+from match import matcher
+
 def main():
     n: int = int(input())
     hospital_preferences: list[list[int]] = []
     student_preferences: list[list[int]] = []
     
     for i in range(n):
-        preferences: list[int] = [int(n) for n in input().strip().split(" ")]
+        preferences: list[int] = [int(n) - 1 for n in input().strip().split(" ")]
         hospital_preferences.append(preferences)
         
     for i in range(n):
-        preferences: list[int] = [int(n) for n in input().strip().split(" ")]
+        preferences: list[int] = [int(n) - 1 for n in input().strip().split(" ")]
         student_preferences.append(preferences)
     
     # call matcher
+    match, _ = matcher(n, hospital_preferences, student_preferences)
+    # output match, 0 indexed so +1
+    for h in range(n):
+        print(h + 1, match[h] + 1)
+
     # call validator
     
 if __name__ == "__main__":
