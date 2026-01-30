392920480b66bb1673007d2ee9346bde3587b671
lchin <l.chin@ufl.edu>
2026-01-30 12:32:26 -0500

    Scalability

--- src/scalability.py
+++ src/scalability.py
@@ -1,9 +1,11 @@
 import time
 import random
+import matplotlib.pyplot as plt
 
 from match import matcher
+from verify import verify
 
-def random_pairs(n: int):
+def random_preferences(n: int):
     l = list(range(1, n + 1))
     hospital_prefs = []
     student_prefs = []
@@ -21,3 +23,37 @@ def random_pairs(n: int):
     return hospital_prefs, student_prefs
 
 def scalability():
+    n_values = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+    match_times = []
+    verify_times = []
+    
+    for n in n_values:
+        hospital_preferences, student_preferences = random_preferences(n)
+        
+        match_start_time = time.time()
+        matched_pairs, proposals = matcher(n, hospital_preferences, student_preferences) # index: hospital, value: student
+        match_end_time = time.time()
+        match_duration = match_end_time - match_start_time
+        
+        verify_start_time = time.time()
+        valid = verify(matched_pairs, hospital_preferences, student_preferences)
+        verify_end_time = time.time()
+        verify_duration = verify_end_time - verify_start_time
+        
+        match_times.append(match_duration)
+        verify_times.append(verify_duration)
+        
+    plt.scatter(n_values, match_times, label='Matching Time', color='blue')
+    plt.xlabel("Number of Hospitals/Students")
+    plt.ylabel("Running Time (seconds)")
+    plt.title("Matching: Number of Hospitals/Students vs. Elapsed Time")
+    plt.savefig("matching_time_plot.png")
+    plt.clf()
+    
+    plt.scatter(n_values, verify_times, label='Verification Time', color='orange')
+    plt.xlabel("Number of Hospitals/Students")
+    plt.ylabel("Running Time (seconds)")
+    plt.title("Verification: Number of Hospitals/Students vs. Elapsed Time")
+    plt.savefig("verification_time_plot.png")
+    
+scalability()
